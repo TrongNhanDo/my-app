cc8fb13e27fb95dda6414c5154427b55dcc9d5b9
dtnhan <dotrongnhan15102000@gmail.com>
2023-06-09 11:10:56 +0700

    fix: add footer and update homepage for admin

--- src/components/Admin/HomePage/index.tsx
+++ src/components/Admin/HomePage/index.tsx
@@ -10,22 +10,22 @@ export const AdminHome = () => {
 
    const fetchApi = useCallback(async () => {
       const userResponse = await callApi("users", "get").catch((err) =>
-         console.log({ err })
+         console.log({ userError: err })
       );
       const productResponse = await callApi("products", "get").catch((err) =>
-         console.log({ err })
+         console.log({ productError: err })
       );
       const ageResponse = await callApi("ages", "get").catch((err) =>
-         console.log({ err })
+         console.log({ ageError: err })
       );
       const branchResponse = await callApi("branches", "get").catch((err) =>
-         console.log({ err })
+         console.log({ branchError: err })
       );
       const skillResponse = await callApi("skills", "get").catch((err) =>
-         console.log({ err })
+         console.log({ skillError: err })
       );
       const roleResponse = await callApi("roles", "get").catch((err) =>
-         console.log({ err })
+         console.log({ roleError: err })
       );
 
       setData({
@@ -47,83 +47,87 @@ export const AdminHome = () => {
    return (
       <div className="container text-dark">
          {showLoader && <Loader />}
-         <Link
-            to="/admin/user-list"
-            className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-blue-400 w-1/4"
-         >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-               User Accounts Management
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-               Total of accounts: {data ? data.users.length : 0}
-            </p>
-         </Link>
-         <Link
-            to="/admin/role-list"
-            className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-amber-400 w-1/4"
-         >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-               Roles Management
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-               Total of accounts: {data ? data.roles.length : 0}
-            </p>
-         </Link>
-         <Link
-            to="/admin/product-list"
-            className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-green-400 w-1/4"
-         >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-               Products Management
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-               Total of products: {data ? data.products.length : 0}
-            </p>
-         </Link>
-         <Link
-            to="/admin/age-category-list"
-            className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-orange-400 w-1/4"
-         >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-               Age Category Management
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-               Total of ages: {data ? data.ages.length : 0}
-            </p>
-         </Link>
-         <Link
-            to="/admin/branch-category-list"
-            className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-pink-400 w-1/4"
-         >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-               Branch Category Management
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-               Total of branches: {data ? data.branches.length : 0}
-            </p>
-         </Link>
-         <Link
-            to="/admin/skill-category-list"
-            className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-purple-400 w-1/4"
-         >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-               Skill Category Management
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-               Total of skills: {data ? data.skills.length : 0}
-            </p>
-         </Link>
-         <Link
-            to="/admin/order-list"
-            className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-red-400 w-1/4"
-         >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-               Orders Management
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-               Total of orders: {data ? data.orders.length : 0}
-            </p>
-         </Link>
+         <div className="grid grid-cols-4 gap-4">
+            <Link
+               to="/admin/user-list"
+               className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-blue-400 w-full"
+            >
+               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  User Accounts Management
+               </h5>
+               <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Total of accounts: {data ? data.users.length : 0}
+               </p>
+            </Link>
+            <Link
+               to="/admin/role-list"
+               className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-amber-400 w-full"
+            >
+               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Roles Management
+               </h5>
+               <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Total of accounts: {data ? data.roles.length : 0}
+               </p>
+            </Link>
+            <Link
+               to="/admin/product-list"
+               className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-green-400 w-full"
+            >
+               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Products Management
+               </h5>
+               <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Total of products: {data ? data.products.length : 0}
+               </p>
+            </Link>
+            <Link
+               to="/admin/age-category-list"
+               className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-orange-400 w-full"
+            >
+               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Age Category Management
+               </h5>
+               <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Total of ages: {data ? data.ages.length : 0}
+               </p>
+            </Link>
+         </div>
+         <div className="grid grid-cols-4 gap-4">
+            <Link
+               to="/admin/branch-category-list"
+               className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-pink-400 w-full"
+            >
+               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Branch Category Management
+               </h5>
+               <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Total of branches: {data ? data.branches.length : 0}
+               </p>
+            </Link>
+            <Link
+               to="/admin/skill-category-list"
+               className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-purple-400 w-full"
+            >
+               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Skill Category Management
+               </h5>
+               <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Total of skills: {data ? data.skills.length : 0}
+               </p>
+            </Link>
+            <Link
+               to="/admin/order-list"
+               className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-400 inline-block m-10 bg-red-400 w-full"
+            >
+               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Orders Management
+               </h5>
+               <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Total of orders: {data ? data.orders.length : 0}
+               </p>
+            </Link>
+         </div>
       </div>
    );
 };
