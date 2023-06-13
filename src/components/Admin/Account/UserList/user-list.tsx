3b6506c14f67b60bf2f607d7d7bc1ac9fcdb3d2c
dtnhan <dotrongnhan15102000@gmail.com>
2023-06-13 14:06:25 +0700

    fix: implement insert modal in users list

--- src/components/Admin/Account/UserList/user-list.tsx
+++ src/components/Admin/Account/UserList/user-list.tsx
@@ -1,18 +1,41 @@
 import { useCallback, useEffect, useMemo, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
 import {
    ActionType,
    ActionTypes,
    InitStateType,
+   InputInsertType,
+   RoleType,
    UserType,
 } from "../common/types";
 import { callApi } from "../../../../api/callApi/callApi";
 import { formatDate } from "../../../Common/Logic/logics";
 import { Loader } from "../../../Common/Loader/loader";
+import { validationSchema } from "./validations";
+import { ModalCustom } from "../../../Common/Modal/modal-custom";
+import { Input } from "../../../Common/Input/input";
 
 export const UserList = () => {
    const [showLoader, setShowLoader] = useState<boolean>(true);
    const dataPerPage = parseInt(import.meta.env.VITE_PER_PAGE || 10);
+   const [success, setSuccess] = useState<boolean>(false);
+   const [error, setError] = useState<string>("");
+   const [modal, setModal] = useState<boolean>(false);
+   const [roleValues, setRoleValues] = useState<RoleType[]>([]);
+
+   const fetchRole = useCallback(async () => {
+      const response = await callApi("roles", "get").catch((err) =>
+         console.log({ err })
+      );
+      setRoleValues(response.data ? response.data : []);
+      setShowLoader(false);
+   }, []);
+
+   useEffect(() => {
+      fetchRole();
+   }, [fetchRole]);
+
    const reducer = (state: InitStateType, action: ActionType) => {
       const { type, payload } = action;
       switch (type) {
@@ -79,6 +102,80 @@ export const UserList = () => {
       return buttons.length > 0 ? buttons : [];
    }, [changePage, data, dataPerPage]);
 
+   const onSubmit = async (formikValues: InputInsertType) => {
+      setSuccess(false);
+      if (
+         formikValues.password &&
+         formikValues.confirmPwd &&
+         formikValues.password !== formikValues.confirmPwd
+      ) {
+         formikBag.setFieldError(
+            "confirmPwd",
+            "Confirm password not matched with password"
+         );
+      } else {
+         setShowLoader(true);
+         const requestPayload = {
+            ...formikValues,
+            username: formikValues.username.trim(),
+            password: formikValues.password.trim(),
+         };
+         await callApi("users", "post", requestPayload)
+            .then(() => {
+               setError("Insert new account success");
+               setSuccess(true);
+            })
+            .catch((err) => {
+               setError(err.response.data.message || "");
+            });
+         setShowLoader(false);
+      }
+   };
+
+   const formikBag = useFormik({
+      initialValues: {
+         username: "",
+         password: "",
+         confirmPwd: "",
+         roleId: 1,
+      },
+      validationSchema,
+      onSubmit,
+   });
+
+   const handleSubmit = useCallback(() => {
+      try {
+         formikBag.submitForm();
+      } catch (error) {
+         console.log({ error });
+      }
+   }, [formikBag]);
+
+   const handleClose = useCallback(() => {
+      try {
+         formikBag.resetForm();
+         setModal(false);
+         setError("");
+      } catch (error) {
+         console.log({ error });
+      }
+   }, [formikBag]);
+
+   useEffect(() => {
+      if (modal) {
+         document.body.style.overflow = "hidden";
+      } else {
+         document.body.style.overflow = "unset";
+      }
+   }, [modal]);
+
+   useEffect(() => {
+      if (success) {
+         formikBag.resetForm();
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [success]);
+
    useEffect(() => {
       fetchApi();
    }, [fetchApi]);
@@ -91,16 +188,134 @@ export const UserList = () => {
                LIST OF USER ACCOUNTS
             </h2>
             <div className="flex justify-center mb-2">
-               <Link
-                  to="/admin/add-user"
+               <button
                   type="button"
                   className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                  onClick={() => setModal(true)}
                >
                   INSERT NEW ACCOUNT
-               </Link>
+               </button>
+               {modal && (
+                  <ModalCustom>
+                     <form onSubmit={formikBag.handleSubmit} className="w-96">
+                        <div className="flex w-full justify-between text-2xl font-bold">
+                           <div className="uppercase">add new role</div>
+                           <button type="button" onClick={handleClose}>
+                              ❌
+                           </button>
+                        </div>
+                        <hr className="w-full my-3 h-0.5" />
+                        {error && error !== "" && (
+                           <div className="bg-lime-300 w-full text-orange-600 mt-4 py-2 px-5 rounded-md">
+                              {error}
+                           </div>
+                        )}
+                        <div className="flex flex-col w-full">
+                           <div className="mb-6">
+                              <Input
+                                 label="Username:"
+                                 name="username"
+                                 id="username"
+                                 type="text"
+                                 className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-1 ${
+                                    formikBag.errors.username &&
+                                    formikBag.touched.username
+                                       ? "bg-yellow"
+                                       : ""
+                                 }`}
+                                 onChange={formikBag.handleChange}
+                                 value={formikBag.values.username || ""}
+                              />
+                              {formikBag.errors.username &&
+                                 formikBag.touched.username && (
+                                    <p className="text-orange-600">
+                                       {formikBag.errors.username}
+                                    </p>
+                                 )}
+                           </div>
+                           <div className="mb-6">
+                              <Input
+                                 label="Password:"
+                                 name="password"
+                                 id="password"
+                                 type="password"
+                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                 onChange={formikBag.handleChange}
+                                 value={formikBag.values.password || ""}
+                              />
+                              {formikBag.errors.password &&
+                                 formikBag.touched.password && (
+                                    <p className="text-orange-600">
+                                       {formikBag.errors.password}
+                                    </p>
+                                 )}
+                           </div>
+                           <div className="mb-6">
+                              <Input
+                                 label="Confirm Password:"
+                                 name="confirmPwd"
+                                 id="confirmPwd"
+                                 type="password"
+                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                 onChange={formikBag.handleChange}
+                                 value={formikBag.values.confirmPwd || ""}
+                              />
+                              {formikBag.errors.confirmPwd &&
+                                 formikBag.touched.confirmPwd && (
+                                    <p className="text-orange-600">
+                                       {formikBag.errors.confirmPwd}
+                                    </p>
+                                 )}
+                           </div>
+                           <div className="mb-6">
+                              <label htmlFor="roleId">Role:</label>
+                              <select
+                                 id="roleId"
+                                 name="roleId"
+                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-base"
+                                 onChange={formikBag.handleChange}
+                                 value={formikBag.values.roleId}
+                              >
+                                 {roleValues &&
+                                    roleValues.map((value, index) => (
+                                       <option key={index} value={value.roleId}>
+                                          {value.roleName}
+                                       </option>
+                                    ))}
+                              </select>
+                              {formikBag.errors.roleId &&
+                                 formikBag.touched.roleId && (
+                                    <p className="text-orange-600">
+                                       {formikBag.errors.roleId}
+                                    </p>
+                                 )}
+                           </div>
+                        </div>
+                        <div className="flex w-full flex-col mt-auto">
+                           <hr className="w-full my-3 h-0.5" />
+                           <div className="flex justify-end">
+                              <button
+                                 type="button"
+                                 className="block bg-blue-500 p-2 rounded font-bold"
+                                 onClick={handleSubmit}
+                              >
+                                 Submit
+                              </button>
+                              <button
+                                 type="button"
+                                 className="block bg-red-500 p-2 rounded font-bold ms-5"
+                                 onClick={handleClose}
+                              >
+                                 Close
+                              </button>
+                           </div>
+                        </div>
+                     </form>
+                  </ModalCustom>
+               )}
             </div>
             {Pagination.length > 1 && <div className="flex">{Pagination}</div>}
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg  m-auto">
+            <div className="overflow-x-auto shadow-md sm:rounded-lg  m-auto">
                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                   <thead className="text-xs text-gray-700 uppercase bg-gray-300">
                      <tr>
