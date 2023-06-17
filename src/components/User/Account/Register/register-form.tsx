49365d94f64b604f517b401b394c3c639ae84f10
dtnhan <dotrongnhan15102000@gmail.com>
2023-06-17 15:48:34 +0700

    fix: update register form for user

--- src/components/User/Account/Register/register-form.tsx
+++ src/components/User/Account/Register/register-form.tsx
@@ -1,37 +1,35 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useFormik } from "formik";
 import { Link } from "react-router-dom";
 import { callApi } from "../../../../api/callApi/callApi";
 import { validationSchema } from "./validations";
-
-type FormikValueType = {
-   email: string;
-   password: string;
-   confirmPwd: string;
-};
+import { FormikValueType, initValueFormik } from "./types";
+import Loader from "../../../Common/Loader/loader";
 
 const LoginForm = React.memo(() => {
-   const initValueFormik: FormikValueType = {
-      email: "",
-      password: "",
-      confirmPwd: "",
-   };
+   const [showLoading, setShowLoading] = useState<boolean>(false);
+   const [checkPass, setCheckPass] = useState<boolean>(false);
 
    const onSubmit = useCallback(async (formikValues: FormikValueType) => {
       try {
+         if (formikValues.password !== formikValues.confirmPwd) {
+            setCheckPass(true);
+            return;
+         }
+         setShowLoading(true);
          const payload = {
             username: formikValues.email || "",
             password: formikValues.password || "",
-            roleId: 1
+            roleId: 1,
          };
          const response = await callApi("users", "post", payload).catch((err) =>
             console.log({ err })
          );
-
+         setShowLoading(false);
          if (response) {
-            alert("Login success");
+            alert("Register success");
          } else {
-            alert("Login fail");
+            alert("Register fail");
          }
       } catch (error) {
          console.log({ error });
@@ -44,8 +42,16 @@ const LoginForm = React.memo(() => {
       onSubmit: (value) => onSubmit(value),
    });
 
+   useEffect(() => {
+      if (checkPass) {
+         formikBag.setFieldError("confirmPwd", "Confirm password is incorrect");
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [checkPass]);
+
    return (
       <div className="div-contai w-1/3 bg-white my-10 rounded-lg shadow">
+         {showLoading && <Loader />}
          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <div className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white uppercase text-center">
                đăng ký tài khoản
@@ -108,7 +114,7 @@ const LoginForm = React.memo(() => {
                </div>
                <div>
                   <label
-                     htmlFor="password"
+                     htmlFor="confirmPwd"
                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                      Xác nhận mật khẩu:
@@ -119,18 +125,19 @@ const LoginForm = React.memo(() => {
                      id="confirmPwd"
                      placeholder="Enter confirm password"
                      className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
-                        formikBag.errors.password &&
-                        formikBag.touched.password &&
+                        formikBag.errors.confirmPwd &&
+                        formikBag.touched.confirmPwd &&
                         "bg-yellow"
                      }`}
-                     value={formikBag.values.password || ""}
+                     value={formikBag.values.confirmPwd || ""}
                      onChange={formikBag.handleChange}
                   />
-                  {formikBag.errors.password && formikBag.touched.password && (
-                     <span className="text-red-800">
-                        {formikBag.errors.password}
-                     </span>
-                  )}
+                  {formikBag.errors.confirmPwd &&
+                     formikBag.touched.confirmPwd && (
+                        <span className="text-red-800">
+                           {formikBag.errors.confirmPwd}
+                        </span>
+                     )}
                </div>
                <button
                   type="submit"
