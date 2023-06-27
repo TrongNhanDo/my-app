cfb3320b5ab4b81aff8557b09c8cad7151ad9651
dtnhan <dotrongnhan15102000@gmail.com>
2023-06-27 11:26:19 +0700

    fix: add sum product

--- src/components/User/Layout/Header/header-user.tsx
+++ src/components/User/Layout/Header/header-user.tsx
@@ -49,9 +49,12 @@ const HeaderUser = React.memo(() => {
             <li>
                <Link
                   to="/carts"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 relative"
                >
                   Carts
+                  <span className="absolute px-1 text-white bg-blue-700 rounded-full sum-count font-bold">
+                     10
+                  </span>
                </Link>
             </li>
             <li>
