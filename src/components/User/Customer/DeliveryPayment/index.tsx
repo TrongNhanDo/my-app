0d58b77c4297f9b500418b5d3105c8f7ea5ee183
dtnhan <dotrongnhan15102000@gmail.com>
2023-06-28 10:16:55 +0700

    fix: add translate for payment and delivery page

--- src/components/User/Customer/DeliveryPayment/index.tsx
+++ src/components/User/Customer/DeliveryPayment/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Sidebar from "../../../Common/Sidebar";
+import { useTranslation } from "react-i18next";
 
 const DeliveryPayment = React.memo(() => {
+   const { t } = useTranslation();
+
    return (
       <div className="div-contai flex mt-5">
          <div className="w-1/4">
@@ -9,76 +12,49 @@ const DeliveryPayment = React.memo(() => {
          </div>
          <div className="w-3/4 bg-white rounded p-5 text-justify">
             <div className="uppercase text-2xl font-bold text-center">
-               giao hàng và thanh toán
+               {t("user.payment_delivery.title")}
             </div>
             <div className="flex flex-col mt-5">
-               <div className="text-xl">
-                  Hiện tại, Yay đang có 2 phương thức giao hàng và 3 phương thức
-                  thanh toán như sau:
-               </div>
+               <div className="text-xl">{t("user.payment_delivery.text1")}</div>
                <div className="uppercase font-bold text-xl my-2">
-                  PHƯƠNG THỨC GIAO HÀNG:
+                  {t("user.payment_delivery.heading1")}
                </div>
                <ul className="">
                   <li className="ps-5 flex">
-                     - Giao hỏa tốc trong ngày: Dành cho khách hàng ở thành phố
-                     Hồ Chí Minh. Quý khách vui lòng ghi chú rõ trong đơn hàng
-                     hoặc liên hệ trực tiếp đến Fanpage hoặc hotline sau khi đặt
-                     hàng để chúng tôi có thể ưu tiên giao hàng cho quý khách.
+                     {t("user.payment_delivery.text2")}
                   </li>
-                  <li className="ps-5">
-                     - Giao hàng thông thường: Chúng tôi sẽ tiến hành giao hàng
-                     cho quý khách thông qua các đơn vị vận chuyển trung gian.
-                     Thời gian giao hàng dự kiến cho khách hàng ở TPHCM là 1-2
-                     ngày và khách hàng ngoại tỉnh là 3-4 ngày.
-                  </li>
+                  <li className="ps-5">{t("user.payment_delivery.text3")}</li>
                </ul>
                <div className="uppercase font-bold text-xl my-2">
-                  PHƯƠNG THỨC THANH TOÁN:
+                  {t("user.payment_delivery.heading2")}
                </div>
                <ul className="">
-                  <li className="ps-5">
-                     - Thanh toán tiền mặt khi nhận hàng – COD (áp dụng cho mọi
-                     đơn hàng có địa chỉ nhận hàng ở Việt Nam).
-                  </li>
-                  <li className="ps-5">
-                     - Thanh toán chuyển khoản: Sau khi khách hàng đặt hàng
-                     thành công, hệ thống sẽ gửi email xác nhận đặt hàng gồm có
-                     mã đơn hàng và thông tin chuyển khoản. Khách hàng có thể
-                     chuyển qua iBanking, chuyển tiền mặt ở ngân hàng, chuyển
-                     qua ATM… Khi chuyển quý khách lưu ý ghi rõ mã đơn hàng để
-                     tiện trong việc kiểm tra và cập nhật thanh toán.
-                  </li>
+                  <li className="ps-5">{t("user.payment_delivery.text4")}</li>
+                  <li className="ps-5">{t("user.payment_delivery.text5")}</li>
                   <ul className="ms-10">
-                     <li className="ps-5 py-3">- Thông tin chuyển khoản:</li>
+                     <li className="ps-5 py-3">
+                        {t("user.payment_delivery.text6")}
+                     </li>
                      <ul className="ms-10">
                         <li className="ps-5">
-                           - Tên người nhận: xxxx xxxx xxxx
+                           {t("user.payment_delivery.text7")}
                         </li>
-                        <li className="ps-5">- Số tài khoản: xxxxxxxxxx</li>
-                        <li className="ps-5">- Tên ngân hàng: Ngân hàng x</li>
                         <li className="ps-5">
-                           - Nội dung chuyển khoản: Mã đơn hàng & Số điện thoại
-                           mua hàng
+                           {t("user.payment_delivery.text8")}
+                        </li>
+                        <li className="ps-5">
+                           {t("user.payment_delivery.text9")}
+                        </li>
+                        <li className="ps-5">
+                           {t("user.payment_delivery.text10")}
                         </li>
                      </ul>
                      <li className="ps-5 pt-3">
-                        - Trường hợp quý khách chuyển khoản qua cây ATM không
-                        ghi được nội dung, vui lòng giữ lại biên nhận và gọi vào
-                        hotline 0978899150 hoặc gửi tin nhắn vào fanpage để được
-                        hỗ trợ xác nhận thanh toán. Sau khi đơn hàng được cập
-                        nhật thanh toán thành công sẽ được xử lý và ship đi sớm
-                        nhất có thể.
+                        {t("user.payment_delivery.text11")}
                      </li>
                   </ul>
                   <li className="ps-5 mt-3">
-                     - Thanh toán qua VNPAY: Sau khi khách hàng bấm vào Hoàn tất
-                     đơn hàng, trang web sẽ hiển thị giao diện thanh toán, quý
-                     khách chọn quét mã QR để tiến hành thanh toán. Trong trường
-                     hợp quý khách chưa kịp thanh toán mà lỡ thoát khỏi giao
-                     diện hoặc mã QR hết hạn, vui lòng liên hệ trực tiếp với
-                     fanpage hoặc hotline 0978899150 để được hỗ trợ thanh toán
-                     và xác nhận đơn hàng.
+                     {t("user.payment_delivery.text12")}
                   </li>
                </ul>
             </div>
