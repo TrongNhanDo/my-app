1cb4b244f5343cd645e79ef4572549d42cde8aa4
dtnhan <dotrongnhan15102000@gmail.com>
2023-06-28 10:25:30 +0700

    fix: add translate for terms of service page

--- src/components/User/Customer/Policy/index.tsx
+++ src/components/User/Customer/Policy/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Sidebar from "../../../Common/Sidebar";
+import { useTranslation } from "react-i18next";
 
 const Policy = React.memo(() => {
+   const { t } = useTranslation();
    return (
       <div className="div-contai flex mt-5">
          <div className="w-1/4">
@@ -9,9 +11,11 @@ const Policy = React.memo(() => {
          </div>
          <div className="w-3/4 bg-white rounded p-5 text-justify">
             <div className="uppercase text-2xl font-bold text-center">
-               điều khoản dịch vụ
+               {t("user.term_service.title")}
             </div>
-            <div className="flex mt-5">Nội dung đang được cập nhật</div>
+            <div className="flex mt-5">
+               {t("user.term_service.updating_content")}
+            </div>
          </div>
       </div>
    );
